3d2a1e0337959a4de410750996e95ee283e4a55f
cjgcs <47309800+cjgcs@users.noreply.github.com>
2021-12-16 13:22:43 +0800

    Update B邮费.java

--- 期末模拟/B邮费.java
+++ 期末模拟/B邮费.java
@@ -1,8 +1,8 @@
-package ��ĩ����;
+package ÆÚÄ©²âÊÔ;
 
 import java.util.Scanner;
 
-public class B�ʷ� {
+public class BÓÊ·Ñ {
 
 	public static void main(String[] args) {
 		Scanner kb=new Scanner(System.in);
@@ -35,3 +35,18 @@ public class B�ʷ� {
 		System.out.printf("%.0f",x);
 	}
 	}
+
+
+根据邮件的重量和计算鸭是否选择加急计算邮费。
+计算规则：重量在 1000克以内(包括1000 克)，基本费 8 元。
+超过 1000克的部分，每500克加收超重费4元，不足500克部分按500克计算；
+如果计算鸭选择加急，多收5元
+
+
+输入
+输入一行，包含一个正整数和一个字符，以一个空格分开，分别表示重量（单位为克，不超过 2*105）和是否加急。  
+如果字符是y，说明选择加急；如果字符是n，说明不加急。
+
+
+输出
+输出一行，包含一个整数，表示邮费。
