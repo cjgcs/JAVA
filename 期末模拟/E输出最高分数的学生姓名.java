3067d2261904e80deb94001e026eea1282d80805
cjgcs <47309800+cjgcs@users.noreply.github.com>
2021-12-16 13:23:39 +0800

    Update E输出最高分数的学生姓名.java

--- 期末模拟/E输出最高分数的学生姓名.java
+++ 期末模拟/E输出最高分数的学生姓名.java
@@ -1,8 +1,8 @@
-package ��ĩ����;
+package 期末测试;
 
 import java.util.Scanner;
 
-public class E�����߷�����ѧ������ {
+public class E输出最高分数的学生姓名 {
 
 	public static void main(String[] args) {
 		Scanner kb=new Scanner(System.in);
@@ -19,5 +19,20 @@ public class E�����߷�����ѧ������ {
 		}
 		System.out.println(max);
 	}
-
 }
+
+输入学生的人数，然后再输入每位学生的分数和姓名，求获得最高分数的学生的姓名。
+
+
+输入
+第一行输入一个正整数 N(N≤100)，表示学生人数。接着输入 N 行，每行格式如下： 
+
+分数 姓名  
+分数是一个非负整数，且小于等于100；
+
+姓名为一个连续的字符串，中间没有空格，长度不超过20。  
+
+数据保证最高分只有一位同学。  
+
+输出
+获得最高分数同学的姓名。
